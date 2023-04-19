a36bbfe9e6c4f3e824ce4bd9429752e1c1b1d673
Finn Lennartsson <finn.lennartsson@gmail.com>
2023-04-19 19:29:16 +0900

    fixing for GBG data

--- bids/DCMrawdicomdir_to_DCMdicomdir.sh
+++ bids/DCMrawdicomdir_to_DCMdicomdir.sh
@@ -7,12 +7,12 @@ usage()
   echo "usage: $base subjectID [options]
 Re-arrangement of raw DICOMs into structured DICOMs
 The scripts uses Docker and heudiconv
-- Raw DICOMs are expected to be in $studyfolder/rawdicomdir
 - Arranges DICOMs into labelled folders (incl renaming of dcm files) into $studyfolder/dicomdir
 
 Arguments:
-  sID				Subject ID (i.e. LIMStudyID) 
+  sID				  Subject ID (i.e. LIMStudyID) 
 Options:
+  -DCMfolder   Raw DICOMs (default: $studyfolder/rawdicomdir/sID)
   -h / -help / --help           Print usage.
 "
   exit;
@@ -23,6 +23,21 @@ Options:
 [ $# -ge 1 ] || { usage; }
 command=$@
 sID=$1
+shift
+
+# Default 
+inputdcmfolder = $rawdcmdir/$sID
+
+# Read arguments
+while [ $# -gt 0 ]; do
+    case "$1" in
+	-DCMfolder  shift; inputdcmfolder=$1; ;;
+	-h|-help|--help) usage; ;;
+	-*) echo "$0: Unrecognized option $1" >&2; usage; ;;
+	*) break ;;
+    esac
+    shift
+done
 
 # Define Folders
 codedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -33,4 +48,4 @@ scriptname=`basename $0 .sh`
 
 # Re-arrange DICOMs into dicomdir
 if [ ! -d $dcmdir ]; then mkdir $dcmdir; fi
-dcm2niix -b o -r y -w 1 -o $dcmdir -f sub-$sID/s%2s_%d/%d_%5r.dcm $rawdcmdir/$sID
+dcm2niix -b o -r y -w 1 -o $dcmdir -f sub-$sID/s%2s_%d/%d_%5r.dcm $inputdcmfolder
